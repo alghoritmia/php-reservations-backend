1cc3058ea021c7cf73072a32f1ddaf9106e6e6c5
Walter Luna <walter.luna@gmail.com>
2022-02-03 15:53:21 -0300

    App de Reservas .. Backend v1.0.1

--- controladores/ruta.controlador.php
+++ controladores/ruta.controlador.php
@@ -4,15 +4,13 @@ class ControladorRuta{
 
 	static public function ctrRuta(){
 
-		//return "http://localhost/www/ALGHORITMIA/github/php-reservations-backend/";
-		return "http://sd-1254248-h00006.ferozo.net/";
+		return "./";
 		
 	}
 
 	static public function ctrRutaBackend(){
 
-		//return "http://localhost/www/ALGHORITMIA/github/php-reservations-backend/";
-		return "http://sd-1254248-h00006.ferozo.net/";
+		return "./";
 
 	}
 
